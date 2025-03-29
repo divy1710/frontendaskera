3815d2d1be5d7787c84384770a92e1cb0ebdebbb
Divy Kalathiya <divykalathiya17@gmail.com>
2025-03-29 13:40:05 +0530

    final Backend

--- askera/src/main.jsx
+++ askera/src/main.jsx
@@ -1,27 +1,11 @@
-// main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import App from './App';
 import './index.css';
-import Home from './contexts/Home';
-import Login from './contexts/Login';
-import Layout from './contexts/Layout';
-import { AuthProvider } from './contexts/AuthContext'; // Ensure the path is correct
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} /> {/* Home as the main route */}
-      <Route path="login" element={<Login />} />
-    </Route>
-  )
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+    <App />
+  // </React.StrictMode>
 );
